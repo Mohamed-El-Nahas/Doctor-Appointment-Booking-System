92e360b0751c1266bb9b10e754457862f8937a30
Mohamed-El-Nahas <135478118+Mohamed-El-Nahas@users.noreply.github.com>
2025-01-20 20:00:04 +0200

    Update Login.jsx

--- appointment-frontend/src/pages/Login.jsx
+++ appointment-frontend/src/pages/Login.jsx
@@ -71,11 +71,11 @@ const Login = () => {
           <p>Password</p>
           <input className='border border-zinc-300 rounded w-full p-2 mt-1' type="password" onChange={(e) => setPassword(e.target.value)} value={password} required/>
         </div>
-        <button type='submit' className='bg-primary text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</button>
+        <button type='submit' className='bg-cyan-600 text-white w-full py-2 rounded-md text-base'>{state === 'Sign Up' ? 'Create Account' : 'Login'}</button>
         {
           state === 'Sign Up'
-          ? <p>Already have an account?  <span onClick={() => setState('Login')} className='text-primary underline cursor-pointer'>Login here</span></p>
-          : <p>Create a new account?  <span onClick={() => setState('Sign Up')} className='text-primary underline cursor-pointer'>Click here</span></p>
+          ? <p>Already have an account?  <span onClick={() => setState('Login')} className='text-cyan-600 underline cursor-pointer'>Login here</span></p>
+          : <p>Create a new account?  <span onClick={() => setState('Sign Up')} className='text-cyan-600 underline cursor-pointer'>Click here</span></p>
         }
       </div>
     </form>
